2d29ef140e4ea0da0f7a40259b852eb7dd93d430
eyal <eyalcourse@gmail.com>
2023-08-27 05:25:52 +0300

    added solution ex 3-5

--- frontEnd/js/230821/230821util.js
+++ frontEnd/js/230821/230821util.js
@@ -52,3 +52,42 @@ function are_equal(num1, num2){
     num2 = parseInt(document.getElementById(num2).value);
     alert(num1 === num2 ? `Numbers are equal. The output is ${num1 + 1}` : `Numbers ${num1} and ${num2} are not equal`)
 }
+
+function pop_inpt(capacity = 4, ex = 3){
+    const list = [];
+    for (let index = 0; index < capacity; index++) {
+        element = `param${ex}.${index+1}`
+        list[index] = parseInt(document.getElementById(element).value);
+    }
+    return list;
+}
+
+function get_lowest(){
+    const list = pop_inpt(4, 3);
+    let flag = 0;
+    let smallest = list[0];
+    console.log(list)
+    for (let i = 0; i < list.length; i ++){
+        if (list[i] >= 1 && list[i] <= 5) {flag = 1};
+        if (list[i] < smallest) {smallest = list[i]};
+        console.log(i, list[i], smallest, flag)
+    }
+    alert(flag === 0 ? `The smallest number is ${smallest}` : `There's a number between 1-5, therefore the output is -1`);
+}
+
+function reset (ex){
+    for (let index = 0; index < 4; index++) {
+        element = `param${ex}.${index + 1}`
+        document.getElementById(element).value = "";
+        document.getElementById(element).style.backgroundColor = "white";
+    }
+}
+
+function is_even(){
+    let x = parseInt(document.getElementById("param4").value);
+    alert(x % 2 === 0 ? `${x} is even` : `${x} is odd`)
+}
+
+// function are_equal2(){
+
+// }
